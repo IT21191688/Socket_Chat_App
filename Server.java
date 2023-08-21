57c6a851d4d022b176ff1681ad35e76567701d54
IT21191688 <IT21191688@my.sliit.lk>
2023-08-21 21:49:06 +0530

    Initial commit

---
+++ Server.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+public class Server{
+    private static List<Handler> clients = new ArrayList<>();
+    public static void main(String[] args) {
+        try{
+        ServerSocket serverSocket = new ServerSocket(8000);
+        System.out.println("Server started..., on port 8000");
+
+        while(true){
+            Socket socket = serverSocket.accept();
+            System.out.println("Client connected..."+socket.getInetAddress().getAddress());
+        
+        
+            Handler handler = new Handler(socket);
+            clients.add(handler);
+            new Thread(handler).start();
+
+        }
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    static void broadcast(String message, Handler handler){
+        for(Handler client:clients){
+            if(client!=handler){
+                client.sendMessage(handler.getClientName()+" : "+message);
+            }
+        }
+    }
+
+}
+
+class Handler implements Runnable{
+
+    private BufferedReader bufferedReader;
+    private PrintWriter printWriter;
+    private String clientName;
+    private Socket socket;
+
+
+    public Handler(Socket socket){
+        this.socket=socket;
+    }
+
+    public String getClientName(){
+        return clientName;
+    }
+
+    public void sendMessage(String message){
+        printWriter.println(message);
+    }
+
+    @Override
+    public void run() {
+       try{
+        bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        printWriter = new PrintWriter(socket.getOutputStream(), true);
+
+        printWriter.println("hello!, What is your name?");
+        clientName = bufferedReader.readLine();
+        printWriter.println(clientName+" : Connected...");
+
+        Server.broadcast("has joined",this);
+
+        String message;
+        while((message=bufferedReader.readLine())!=null){
+            Server.broadcast(message, this);
+        }
+
+        Server.broadcast("user left...", this);
+
+        bufferedReader.close();
+        printWriter.close();
+        socket.close();
+
+
+       }catch(IOException e){
+        e.printStackTrace();
+       }
+    }
+
+}
